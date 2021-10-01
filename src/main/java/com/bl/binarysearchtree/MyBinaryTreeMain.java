258f5bf7762a45c32d67fab43e227c1d728e2267
iamsr5675 <saurabhranjan5675@gmail.com>
2021-10-01 17:17:11 +0530

    Ability to search 63

--- src/main/java/com/bl/binarysearchtree/MyBinaryTreeMain.java
+++ src/main/java/com/bl/binarysearchtree/MyBinaryTreeMain.java
@@ -4,8 +4,8 @@ public class MyBinaryTreeMain {
 
 	public static void main(String[] args) {
 
-System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");
-		
+		System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");
+
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
@@ -20,10 +20,11 @@ System.out.println("------ Welcome to Data Structure : Binary Search Tree ------
 		myBinaryTree.add(16);
 		myBinaryTree.add(63);
 		myBinaryTree.add(67);
-		
-		myBinaryTree.printBinaryTree();
-		System.out.println("The Size of the Binary Tree is : "+myBinaryTree.getSize());
 
+		myBinaryTree.printBinaryTree();
+		System.out.println("The Size of the Binary Tree is : " + myBinaryTree.getSize());
+
+		System.out.println("Is 63 present in the Binary Search Tree? " + myBinaryTree.searchTree(63));
 	}
 
 }
